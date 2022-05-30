f86416b90c5e3a28bdebcc2ab10871957e09b36d
zavm07 <zavm07@vse.cz>
2022-05-30 15:41:14 +0200

    item details

--- products.php
+++ products.php
@@ -27,7 +27,6 @@ if(!empty($_GET['category'])){
 
 }
 
-
 if (isset($_GET['offset'])) {
     $offset = (int)$_GET['offset'];
 } else {
@@ -48,17 +47,21 @@ $products = $productsDB->fetchByCategory($cat_id, $offset, $itemsPerPage);
     <?php foreach ($products as $product):?>
         <div class="card m-5" style="width:300px;height: auto">
             <div class="container w-100 p-0">
-                <img class="card-img-top" src="<?php echo $product['img_link'] ?>" alt="Náhled produktu " width="300" height="auto">
+                <a href="item.php?id=<?php echo $product['prod_id'] ?>">
+                    <img class="card-img-top" src="<?php echo $product['img_link'] ?>" alt="Náhled produktu " width="300" height="auto">
+                </a>
             </div>
             <div class="card-body text-black">
                 <h4 class="card-title"><?php echo $product['prod_name'] ?></h4>
-                <p class="card-text overflow-hidden" style="height: 4.5em"><?php echo $product['description'] ?></p>
+                <div class="d-flex flex-column ">
+                    <p class="card-text overflow-hidden mb-0" style="height: 4.5em"><?php echo $product['description'] ?></p>
+                    <a href="item.php?id=<?php echo $product['prod_id'] ?>" class="align-self-end">Více informací</a>
+                </div>
                 <h5><?php echo $product['size'] ?></h5>
                 <h4><?php echo $product['price'] ?> Kč</h4>
                 <form method="post" action="addToCart.php">
                     <div class="d-flex justify-content-between mt-4">
                         <input type="hidden" name="id" value="<?php echo $product['prod_id']?>">
-                        <input type="hidden" name="url" value="<?php echo getcwd() ?>">
                         <button type="submit" class="btn btn-primary">Přidat do košíku</button>
                         <?php if(!empty($_SESSION['lg_privileges'])&&$_SESSION['lg_privileges'] == 2):?>
                             <a href="#" class="btn btn-warning"><img src="res/icons/edit.svg" title="Upravit produkt" alt="Upravit produkt"></a>
@@ -77,7 +80,6 @@ $products = $productsDB->fetchByCategory($cat_id, $offset, $itemsPerPage);
                     Předchozí
                 </a>
             </li>
-
            <?php for ($i = 1; $i <= ceil($count / $itemsPerPage); $i++): ?>
                <li class="page-item">
                    <a class="page-link <?php echo $offset / $itemsPerPage + 1 == $i ? "active" : ""; ?>" href="products.php?offset=<?php echo ($i - 1) * $itemsPerPage; ?>">
@@ -85,7 +87,6 @@ $products = $productsDB->fetchByCategory($cat_id, $offset, $itemsPerPage);
                    </a>
                </li>
             <?php endfor; ?>
-
             <li class="page-item">
                 <a class="page-link <?php echo $offset+$itemsPerPage>= $count ? "disabled" : "";?>" href="products.php?offset=<?php echo ($offset+$itemsPerPage); ?>">
                     Další
